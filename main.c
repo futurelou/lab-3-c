eb8852baaa2c28bedc4f3d8b69a7e76b424d2414
futurelou <lzc5535@psu.edu>
2020-09-17 23:38:59 +0000

    louie

--- main.c
+++ main.c
@@ -28,11 +28,11 @@ int sum_n(int n){
 
 int main(void) { 
 int n;
-char *res = readline("Enter an int:");
+char *res = readline("Enter an int: ");
 int t;
 n = atoi (res); 
 t = sum_n(n);
-printf("sum is %i \n.",t);
+printf("sum is %i \n",t);
 
 char *a = readline("Enter a string: ");
 
